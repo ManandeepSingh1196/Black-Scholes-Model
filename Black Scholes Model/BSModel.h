e32f7708fe31e18e13f81cababd2cf24faebb0fc
maus911 <manan801p@gmail.com>
2025-09-09 14:44:30 +0530

    More work, I have done hmmm (read in yoda voice)

--- Black Scholes Model/BSModel.h
+++ Black Scholes Model/BSModel.h
@@ -11,7 +11,7 @@ private:
 	double riskFreeRate;
 
 public:
-	double N;
+	double K_;
 	double d1;
 	double d2;
 	double strikePrice;
@@ -28,22 +28,29 @@ private:
 
 private:
 	//Getter Functions for the Black-Scholes Model
-	double getD1() const;
-	double getD2() const;
-	double getStrikePrice() const;
+	double getOptionPrice() const;		double getTTM() const;
+	double getVolatility() const;		double getRFR() const;
+
+
+	double getD1() const;		double getD2() const;
+	double getK() const;		double getStrikePrice() const;
 
 	//Setter Functions for the Black-Scholes Model
+	void setOptionPrice(const double& value) const;		void setTTM(const double& value) const;
+	void setVolatility(const double& value) const;		void setRFR(const double& value) const;
+	void setK(const double& value) const;		
+
+	void setStrikePrice(const double& value) const;
 	void setD1(const double& value) const;
 	void setD2(const double& value) const;
 
 	//Calculator Functions for the Black-Scholes Model
-	void calculateD1(double optionPrice, double strikePrice, double timeToMaturity, double riskFreeRate, double volatility);
-	void calculateD2(double optionPrice, double strikePrice, double timeToMaturity, double riskFreeRate, double volatility);
+	double calculateD1(double optionPrice, double strikePrice, double timeToMaturity, double riskFreeRate, double volatility);
+	double calculateD2(double optionPrice, double strikePrice, double timeToMaturity, double riskFreeRate, double volatility);
 
 
 private:
-	//Calculate probability values
-	double normalPDF();
+	//Calculate probability value
 	double normalCDF(double d) const;
 
 public:
